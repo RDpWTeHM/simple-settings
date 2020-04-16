8933bfb87cf8bc4e21adedd244966b2b6257b255
JosephLin <joseph.lin@aliyun.com>
2020-04-16 11:20:33 +0800

    update last fix. last fix missing `()` not fix that issue

--- simple_settings/__init__.py
+++ simple_settings/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-from .core import LazySettings, settings  # noqa
 
 
 if os.getcwd() not in sys.path:
-    sys.path.insert(0, os.getcwd)
+    sys.path.insert(0, os.getcwd())
+
+
+from .core import LazySettings, settings  # noqa
